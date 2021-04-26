35df48c9d8b3785758050dc30de305c3671c39cc
Nikhil Varshney <nikhil.varshney@manprax.com>
2021-04-26 19:12:07 +0530

    fix js not loading issue.

--- website_scorm_elearning/__manifest__.py
+++ website_scorm_elearning/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'eLearning with Scorm',
-    'version': '1.0',
+    'version': '1.1',
     'sequence': 10,
     'summary': 'Manage and publish an eLearning platform',
     'website': 'https://www.manprax.com',
