e433ebca3ffb2a398366da3b3d128048101f1e7c
Manprax Y-490 Nov -2024 <krishnansh.kapoor@manprax.com>
2025-01-29 15:08:39 +0530

    [FIX]Issue for External Library

--- website_scorm_elearning/__manifest__.py
+++ website_scorm_elearning/__manifest__.py
@@ -29,6 +29,7 @@ Create Online Courses Using Scorm
             'website_scorm_elearning/static/src/scss/slide_slide.scss',
         ]
     },
+    'external_dependencies': {'python': ['boto3']},
     'images': ["static/description/images/scorm_banner.png"],
     'installable': True,
     'application': True,
