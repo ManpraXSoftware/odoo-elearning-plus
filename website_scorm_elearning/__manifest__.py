477022f00bb88e32a6b00adbc53c54ee88eacfeb
krishnansh-manprax <krishnansh.kapoor@manprax.com>
2025-07-02 16:36:24 +0530

    [UPD]Logo Changes

--- website_scorm_elearning/__manifest__.py
+++ website_scorm_elearning/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'eLearning with Scorm',
-    'version': '18.4',
+    'version': '18.5',
     'sequence': 10,
     'summary': 'Manage and publish an eLearning platform',
     'website': 'https://www.manprax.com',
