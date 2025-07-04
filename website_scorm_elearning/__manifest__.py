9d0e91d23b05823f55667b1dc6de5b7a79fbe086
Yishu Sinha <yishu.sinha@manprax.com>
2025-07-04 17:36:02 +0530

    [UPD]: updated logo

--- website_scorm_elearning/__manifest__.py
+++ website_scorm_elearning/__manifest__.py
@@ -4,7 +4,7 @@
     'version': '17.0.1.5',
     'sequence': 10,
     'summary': 'Manage and publish an eLearning platform',
-    'icon': '/website_scorm_elearning/static/description/icon.svg',
+    'icon': '/website_scorm_elearning/static/description/icon2.png',
     'website': 'https://www.manprax.com',
     'author': 'ManpraX Software LLP',
     'category': 'Website/eLearning',
