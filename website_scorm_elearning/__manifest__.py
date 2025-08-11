9e44871c0b697ddf92b736b2d72d6682bd0c5abf
Yishu Sinha <yishu.sinha@manprax.com>
2025-08-11 17:27:41 +0530

    [FIX]: updated version

--- website_scorm_elearning/__manifest__.py
+++ website_scorm_elearning/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'eLearning with Scorm',
-    'version': '17.0.1.7',
+    'version': '17.0.1.8',
     'sequence': 10,
     'summary': 'Manage and publish an eLearning platform',
     'website': 'https://www.manprax.com',
